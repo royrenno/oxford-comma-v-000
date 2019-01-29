385b2506e0feb83e9813e1d61d76ef2d73d29156
roy renno <royrenno@yahoo.com>
2019-01-29 04:12:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
-def oxford_comma(array)	
+def oxford_comma(array)
 	if array.length == 2
 	array[-2] << " and "
 	array.join
-	elsif array.length == 1 
+	elsif array.length == 1
 	array.join
-	elsif array.length > 2 
+	elsif array.length > 2
 	array[-1].prepend "and "
-	array.join(", ") 
+	array.join(", ")
 	end
 	end
