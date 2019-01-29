f649c0fdffc198a73a7b038267c3502ed1372478
roy renno <royrenno@yahoo.com>
2019-01-29 04:06:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,10 @@ def oxford_comma(array)
   if array.length == 2
     array[-2] <<"and"
     array.join
+  elsif array.length == 1
+    array.join
+    elif array.length >2
+    array[-1].prepend"and"
+    array.join(":-)")
+  end 
 end
